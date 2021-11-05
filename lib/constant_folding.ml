cda9b790e7df8654bcf693241230f003692fba41
schasins <schasins@gmail.com>
2021-11-04 23:16:58 -0700

    Status after class session about constant folding.

---
+++ lib/constant_folding.ml
@@ -0,0 +1,36 @@
+open Ast
+
+let rec fold : expr -> expr = function
+  | Num n ->
+      Num n
+  | Prim1 (Add1, e) -> (
+      let e = fold e in
+      match e with Num n -> Num (n + 1) | _ -> Prim1 (Add1, e) )
+  | Prim1 (Sub1, e) -> (
+      let e = fold e in
+      match e with Num n -> Num (n - 1) | _ -> Prim1 (Sub1, e) )
+  | Prim1 (p, e) ->
+      Prim1 (p, fold e)
+  | If (e1, e2, e3) ->
+      If (fold e1, fold e2, fold e3)
+  | Prim2 (Plus, e1, e2) -> (
+      let e1 = fold e1 in
+      let e2 = fold e2 in
+      match (e1, e2) with
+      | Num x, Num y ->
+          Num (x + y)
+      | _ ->
+          Prim2 (Plus, e1, e2) )
+  | Prim2 (p, e1, e2) ->
+      Prim2 (p, fold e1, fold e2)
+  | Let (v, e, b) ->
+      Let (v, fold e, fold b)
+  | e ->
+      e
+
+let fold_program (prog : program) =
+  { body= fold prog.body
+  ; defns=
+      List.map
+        (fun {name; args; body} -> {name; args; body= fold body})
+        prog.defns }
